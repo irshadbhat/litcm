599e652e1b521c0ba126f275a0dbb27ca0de33b9
Irshad Bhat <bhatirshad127@gmail.com>
2015-07-31 11:40:51 +0530

    temporary removal

--- convertor-indic-1.4.9/utf2wx_run.sh
+++
@@ -1,8 +0,0 @@
-if [ $# -le 1 ]
-then
-        echo "Error - Arguments missing!!"
-        echo -e "Syntax :\nsh $0 <format-type[text|SSF]> <language[hin|tel|..]> <input-file-name>";
-        echo -e "e.g.:\nsh $0 ssf hin input-file-name";
-exit
-fi
-perl -C $convertorIndic/convertor_indic.pl -f=$1 -l=$2 -s=utf -t=wx -i=$3
